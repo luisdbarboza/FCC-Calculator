21eac07288716605d9eb8e3adb5da5cf594e9499
Luis Barboza <lbarbozanav@gmail.com>
2021-05-22 01:21:32 -0400

    Updates

---
+++ precache-manifest.42e99dae56230f104be3f59c52173475.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "198f41a3e163f1ebb4edbb3de3e8f435",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ecffd3328b5f1c5f8b2",
+    "url": "/static/css/main.311c2e13.chunk.css"
+  },
+  {
+    "revision": "2978b2922123b1520f2b",
+    "url": "/static/js/2.aca13bec.chunk.js"
+  },
+  {
+    "revision": "9ecffd3328b5f1c5f8b2",
+    "url": "/static/js/main.048f31fe.chunk.js"
+  },
+  {
+    "revision": "fc3ae5f4b2a8aa3d9a7c",
+    "url": "/static/js/runtime-main.1ed00a13.js"
+  }
+]);
